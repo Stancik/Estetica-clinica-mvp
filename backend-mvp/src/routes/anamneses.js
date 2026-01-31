f4b3390d91402c4e524f9aac47f8836c4a1d84ea
Stancik <juniorstancik@gmail.com>
2026-01-30 21:25:33 -0300

    Create anamneses.js

---
+++ backend-mvp/src/routes/anamneses.js
@@ -0,0 +1,10 @@
+import { Router } from &#39;express&#39;;
+import { authRequired } from &#39;../utils/auth.js&#39;;
+import { create } from &#39;../controllers/anamneses.js&#39;;
+
+const router = Router();
+router.use(authRequired);
+
+router.post(&#39;/&#39;, create);
+
+export default router;
